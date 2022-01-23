7ed7303dfab4a7d1e3a6729a4fde83d450bc2697
hamza-at-youcode <hamzayocude@gmail.com>
2022-01-23 15:13:52 +0100

    Fixe logout

--- logout.php
+++ logout.php
@@ -2,9 +2,10 @@
 
 session_start();
 
-if (isset($_SESSION['adminID'])) {
-	unset($_SESSION['adminID']);
+if (isset($_SESSION['logedIn'])) {
+	session_unset();
+	session_destroy();
 }
 
 header("Location: login.php");
-die;
+exit;
