febeec91348b6b9991379b627b1a74bde4b870de
hamza-at-youcode <hamzayocude@gmail.com>
2022-02-08 14:10:56 +0100

    Add get the line number option to dump function

--- dump.php
+++ dump.php
@@ -1,8 +1,9 @@
 <?php
 
 
-function dump($var){
+function dump($var = 'line'){
     echo '<pre>';
+    if($var == 'line') {echo __LINE__;return;}
     if(is_string($var)) echo $var.'<br>';
     if(is_array($var)) print_r($var);
 }
